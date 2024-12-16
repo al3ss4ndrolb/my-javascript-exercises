84299d28f20cffc5b5d1df5764247cc84331a489
al3ss4ndrolb <173555546+al3ss4ndrolb@users.noreply.github.com>
2024-12-16 17:27:05 +0100

    fix: finish exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,25 @@
-const repeatString = function() {
+const repeatString = function(string, times) {
+    let result = '';
 
+    if (times < 0) {
+        return 'ERROR';    
+    } 
+
+    if (string === null) {
+        return 'ERROR';
+    }
+
+    for (let i = 0; i < times; i++){
+        result += string;
+    }
+        
+    return result
 };
+
+// console.log(repeatString("hey", 3));
+
+// Do not edit below this line
+module.exports = repeatString;
 
 // Do not edit below this line
 module.exports = repeatString;
