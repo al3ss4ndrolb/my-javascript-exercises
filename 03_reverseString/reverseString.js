851dc18e89138491ee06c7e418141d4f2907863f
al3ss4ndrolb <173555546+al3ss4ndrolb@users.noreply.github.com>
2024-12-16 18:14:06 +0100

    fix: finish exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,23 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    let length = word.length//misura lunghezza word
+    let myArray = [];
 
+    if (length === 0) 
+        return ""; 
+    
+    for (let i = length-1; i >= 0; i--) {
+        let char = word.charAt(i);
+
+        
+        myArray.push(char); //per ogni carattere mettila dentro una lista
+    }
+
+    //console.log(myArray);
+    return myArray.join("");
+    //ritorna lista al contrario, dall'ultimo carattere
 };
+
+reverseString("culo")
 
 // Do not edit below this line
 module.exports = reverseString;
