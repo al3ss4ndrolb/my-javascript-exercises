8d19d5fe84abc6e0bb72aa9808112356d67614ce
al3ss4ndrolb <173555546+al3ss4ndrolb@users.noreply.github.com>
2024-12-18 15:54:05 +0100

    updaTE: finish exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+    let resultCelsius = (temperature-32)*5/9; 
+    return Math.round(resultCelsius*10)/10;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let resultFahrentheit= temperature * 9/5 + 32;
+  return Math.round(resultFahrentheit*10)/10;
 };
+
+
 
 // Do not edit below this line
 module.exports = {
