b37134e2a5ba1d006c81b1d2975d6d8a1cdf9f8a
al3ss4ndrolb <173555546+al3ss4ndrolb@users.noreply.github.com>
2024-12-18 13:39:15 +0100

    update: finish exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(firstInt, lastInt) {
+    let result = 0; // declare variable to hold final sum   
+    
+    if (firstInt <=0 || 
+        lastInt <=0 || 
+        typeof firstInt != "number" || 
+        typeof lastInt != "number" || 
+        !Number.isInteger(firstInt) || 
+        !Number.isInteger(lastInt)) {
+        return "ERROR"
+    }
+    
+    const start = Math.min(firstInt, lastInt);
+    const end = Math.max(firstInt, lastInt);
 
+    for (let i = start; i <= end; i++) {
+        result += i;
+    }
+
+    return result
 };
 
 // Do not edit below this line
