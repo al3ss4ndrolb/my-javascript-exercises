df2c5d2a3990f07675c8f78b5d575e2e980edef1
al3ss4ndrolb <173555546+al3ss4ndrolb@users.noreply.github.com>
2024-12-28 01:44:50 +0100

    feat: add function to exercise to finish it

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const currentYear = new Date().getFullYear();
 
+  return people.reduce((oldest, person) => {
+    // Safely handle missing properties
+    const personAge = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+    const oldestAge = oldest
+      ? (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth
+      : 0;
+
+    return personAge > oldestAge ? person : oldest;
+  }, null);
 };
 
 // Do not edit below this line
